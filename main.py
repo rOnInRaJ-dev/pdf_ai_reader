31582debf5b09d8ac4f4adf84749b3ca63c9051d
u7527120 <u7527120@anu.edu.au>
2024-07-04 01:02:17 +1000

    Uses openai api, and reads text and understands it

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+
 
 def main():
     load_dotenv()
@@ -8,10 +15,11 @@ def main():
     # UI STUFF
     st.set_page_config(page_title="Ask your pdf")
     st.header("Ask your PDF")
+
+    # upload file
     pdf = st.file_uploader("Upload your pdf", type="pdf")
 
-
-
+    # reading pdf file
     if pdf is not None:
         # Reading the pdf file
         pdf_reader = PdfReader(pdf)
@@ -19,8 +27,37 @@ def main():
         for page in pdf_reader.pages:
             text += page.extract_text()
 
+        # split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
 
-        st.write(text)
+        chunks = text_splitter.split_text(text)
+
+        # Create embeddings
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+        # show user input
+        user_question = st.text_input("Ask a question:")
+        if user_question:
+            docs = knowledge_base.similarity_search(user_question)
+
+            llm = OpenAI()
+            chain = load_qa_chain(llm, chain_type="stuff")
+
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=user_question)
+                print(cb)
+
+            st.write(response)
+
+
+
+
 
 
 
